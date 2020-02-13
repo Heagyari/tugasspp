ebc7a18f47fcf70888bacc7c9a4678708be13622
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2020-02-13 11:52:49 +0700

    Add files via upload

---
+++ tambah_aksi.php
@@ -0,0 +1,20 @@
+<?php
+    // Connection
+    require 'koneksi.php';
+
+    // Terima data
+    $nisn = $_POST['nisn'];
+    $nis = $_POST['nis'];
+    $nama = $_POST['nama'];
+    $id_kelas = $_POST['id_kelas'];
+    $alamat = $_POST['alamat'];
+    $no_telp = $_POST['no_telp'];
+    $id_spp = $_POST['id_spp'];
+
+    // var_dump($id_spp);
+
+    mysqli_query($koneksi, "insert into siswa values('$nisn', '$nis', '$nama', '$id_kelas', '$alamat', '$no_telp', '$id_spp')");
+
+    // Redirect
+    header('location:index.php');
+?>
